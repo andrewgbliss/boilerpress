fdcb4fdd4e749acf4089f9596ffa01db6f046e5a
Andrew Bliss <andrewgbliss@gmail.com>
2018-09-28 11:07:55 -0600

    morgan

--- src/lib/index.js
+++ src/lib/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('port', process.env.PORT || '3000');
 app.set('env', process.env.NODE_ENV || 'development');
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
